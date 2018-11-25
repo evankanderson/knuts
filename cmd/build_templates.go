ec9321182eb067adcd947f73b2c506091b5dfedc
Evan Anderson <evan.k.anderson@gmail.com>
2018-11-24 16:43:48 -0800

    Enable both short flags and menu-based chooser.
    Signed-off-by: Evan Anderson <evan.k.anderson@gmail.com>

--- cmd/build_templates.go
+++ cmd/build_templates.go
@@ -3,7 +3,9 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"strings"
 
+	"github.com/AlecAivazis/survey"
 	"github.com/evankanderson/knuts/pkg"
 	"github.com/evankanderson/knuts/pkg/builds"
 	"github.com/spf13/cobra"
@@ -11,21 +13,67 @@ import (
 
 func init() {
 	rootCmd.AddCommand(buildTemplateCmd)
+	selected = buildTemplateCmd.PersistentFlags().StringSlice(
+		"templates", []string{}, "Names of build templates to install. If not provided, will prompt interactively.")
 }
+
+var byName = prepare(builds.Builds)
+
+var selected *[]string
 
 var buildTemplateCmd = &cobra.Command{
 	Use:     "build_templates",
 	Aliases: []string{"builds", "bt"},
 	Short:   "Menu-guided install of build templates.",
 	Run: func(cmd *cobra.Command, args []string) {
+		if len(*selected) == 0 {
+			selected = fromPrompt(byName)
+		}
 		if err := pkg.Installed("kubectl"); err != nil {
 			fmt.Print(err)
 			os.Exit(2)
 		}
 
-		fmt.Println("Build template options:")
-		for _, b := range builds.Builds {
-			fmt.Printf("  %s\n", b.Desc)
+		installs := []builds.BuildTemplate{}
+		yamls := []string{}
+		for _, n := range *selected {
+			b := byName[n]
+			installs = append(installs, b)
+			yamls = append(yamls, b.URL)
 		}
+
+		fmt.Printf("Got the following builds: %s\n", yamls)
 	},
+}
+
+func prepare(templates []builds.BuildTemplate) map[string]builds.BuildTemplate {
+	byName := map[string]builds.BuildTemplate{}
+	for _, b := range builds.Builds {
+		byName[b.Short] = b
+	}
+	return byName
+}
+
+func fromPrompt(m map[string]builds.BuildTemplate) *[]string {
+	buildChoices := make([]string, len(byName))
+	i := 0
+	for _, v := range byName {
+		buildChoices[i] = fmt.Sprintf("%s: %s", v.Short, v.Desc)
+		i++
+	}
+	prompt := &survey.MultiSelect{
+		Message: "Which BuildTemplates do you want to install?",
+		Options: buildChoices,
+	}
+	answers := []string{}
+	err := survey.AskOne(prompt, &answers, nil)
+	if err != nil {
+		fmt.Printf("Error prompting for builds: %s\n", err)
+		os.Exit(1)
+	}
+	ret := make([]string, len(answers))
+	for i, s := range answers {
+		ret[i] = strings.SplitN(s, ":", 2)[0]
+	}
+	return &ret
 }
--- pkg/builds/templates.go
+++ pkg/builds/templates.go
@@ -2,7 +2,9 @@ package builds
 
 // BuildTemplate represents the steps needed to install a particular BuildTemplate.
 type BuildTemplate struct {
-	//Desc contains a human-readable short description of the BuildTemplate.
+	// Short is a single-word command-line flag name for the BuildTemplate.
+	Short string
+	// Desc contains a human-readable short description of the BuildTemplate.
 	Desc string
 	// URL of the current (YAML) definition of this template.
 	URL string
@@ -13,21 +15,25 @@ type BuildTemplate struct {
 // Builds contains the set of known BuildTemplates.
 var Builds = []BuildTemplate{
 	{
+		Short:   "jib-gradle",
 		Desc:    "Gradle build with JIB",
 		URL:     "https://github.com/knative/build-templates/blob/master/jib/jib-gradle.yaml",
 		Secrets: []SecretType{ImagePush},
 	},
 	{
+		Short:   "jib-maven",
 		Desc:    "Maven build with JIB",
 		URL:     "https://github.com/knative/build-templates/blob/master/jib/jib-maven.yaml",
 		Secrets: []SecretType{ImagePush},
 	},
 	{
+		Short:   "kaniko",
 		Desc:    "Dockerfile with Kaniko",
 		URL:     "https://github.com/knative/build-templates/blob/master/kaniko/kaniko.yaml",
 		Secrets: []SecretType{ImagePush},
 	},
 	{
+		Short:   "buildpack",
 		Desc:    "Buildpack",
 		URL:     "https://github.com/knative/build-templates/blob/master/buildpack/buildpack.yaml",
 		Secrets: []SecretType{ImagePush},
